ba4928796c478fbf95f60cc60440380343a86ded
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-07-20 04:03:53 +0600

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,76 @@
+<script>
+  import countries_services from "./services/countries";
+  import LoadingScreen from "./components/LoadingScreen";
+  import Card from "./components/Card";
+
+  let countries = [];
+  let filters = [];
+  let searchVal = "";
+  let message = "";
+
+  const populate = async () => {
+    countries = await countries_services.getAllCountries();
+  };
+
+  $: {
+    const match = new RegExp(searchVal.trim(), "i");
+    filters = countries.filter(c => match.test(c.name.official));
+
+    if (filters.length > 10) {
+      message = "";
+      filters = [];
+    } else {
+      message = `Found ${filters.length} results`;
+    }
+  }
+</script>
+
+<style>
+  main {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  :global(body) {
+    background-color: orange;
+  }
+
+  h1,
+  p {
+    padding-top: 20px;
+  }
+
+  input {
+    width: 300px;
+  }
+</style>
+
+<main>
+    {#await populate()}
+      <LoadingScreen />
+    {:then results}
+      <h1>Search a country</h1>
+      <input bind:value={searchVal} />
+      {#if message}
+        <p>{message}</p>
+      {/if}
+
+      {#each filters as filter (filter.name.common)}
+        <Card 
+          imageSource={filter.flags.png} 
+          name={filter.name.common}
+          capital={filter.capital[0]}
+          population={filter.population}
+          continent={filter.continents[0]}
+          timezone={filter.timezones[0]}
+          map={filter.maps.googleMaps} />
+      {/each}
+
+    {:catch error}
+      <p>{error.message}</p>
+    {/await}
+    
+</main>
---
+++ src/services/countries.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const url = "https://restcountries.com/v3.1";
+
+const getAllCountries = async () => {
+  try {
+    const response = await axios.get(`${url}/all`);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Failed to load. Code: ${error.response.status}`);
+  }
+};
+
+export default {
+  getAllCountries
+};
